a4fab42b4427cb8b0572a0d8270a96efcab3b6d8
sujeongHeo <gjtnwjd40@gmail.com>
2021-05-03 01:45:59 +0900

    고루틴 사용

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-type result struct {
+type requestResult struct {
 	url    string
 	status string
 }
@@ -14,8 +14,8 @@ type result struct {
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	// results := make(map[string]string)
-	c := make(chan result)
+	results := make(map[string]string)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -25,20 +25,28 @@ func main() {
 		"https://www.instagram.com",
 		"https://academy.nomadcoders.co/",
 	}
-	// results["hello"] = "Hello"
+	// requestResult["hello"] = "Hello"
 	for _, url := range urls {
 		go hitURL(url, c)
 	}
 
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+
 }
 
-func hitURL(url string, c chan<- result) {
-	fmt.Println("Checking: ", url)
+func hitURL(url string, c chan<- requestResult) {
 	resp, err := http.Get(url)
 	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
 		status = "FAILED"
 	}
-	c <- result{url: url, status: status}
+	c <- requestResult{url: url, status: status}
 
 }
