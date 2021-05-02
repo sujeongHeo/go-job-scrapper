86fb895f2871cb84dd92617a0032fedefa57e120
sujeongHeo <gjtnwjd40@gmail.com>
2021-05-02 19:24:09 +0900

    429 는 TooManyRequest

--- main.go
+++ main.go
@@ -21,14 +21,23 @@ func main() {
 	}
 	results["hello"] = "Hello"
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
 func hitURL(url string) error {
 	fmt.Println("Checking: ", url)
 	resp, err := http.Get(url)
-	if err == nil || resp.StatusCode >= 400 {
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errRequestFailed
 	}
 	return nil
