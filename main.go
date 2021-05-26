003f3a12f8734b43a36e54bc3e3c8031f9052c72
sujeongHeo <gjtnwjd40@gmail.com>
2021-05-26 13:44:26 +0900

    download file

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "github.com/sujeongHeo/learngo/scrapper"
+import (
+	"os"
+	"strings"
+
+	"github.com/labstack/echo"
+	"github.com/sujeongHeo/learngo/scrapper"
+)
+
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
+}
+
+// func CleanString(str string) string {
+// 	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+// }
 
 func main() {
-	scrapper.Scrape("term")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
